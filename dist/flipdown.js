8adc7e1f9ce63b3bf1fe24871ef36db93dd0f819
pbutcher <pbutcher93@gmail.com>
2018-09-28 14:51:02 +0100

    Adds minified builds

--- dist/flipdown.js
+++ dist/flipdown.js
@@ -1,155 +1,86 @@
-/**
-* @name FlipDown
-* @version 0.1.2
-* @description Flip styled countdown clock
-* @author Peter Butcher (PButcher) <pbutcher93[at]gmail[dot]com>
-* @param {number} t - Time to count down to as unix timestamp
-* @param {string} el - DOM element to attach FlipDown to
-**/
+"use strict";
+
 function FlipDown(t, el) {
-
-  // FlipDown Version
-  this.version = '0.1.2';
-
-  // Initialised?
+  this.version = '0.1.3';
   this.initialised = false;
-
-  // Time at instantiation in seconds
   this.now = this.getTime();
-
-  // UTS to count down to
   this.epoch = t;
-
-  // UTS passed to FlipDown is in the past
-  this.countdownEnded = (this.epoch - this.now) < 0;
-
-  // FlipDown DOM element
+  this.countdownEnded = this.epoch - this.now < 0;
   this.element = document.getElementById(el);
-
-  // Rotor DOM elements
   this.rotors = [];
   this.rotorLeafFront = [];
   this.rotorLeafRear = [];
   this.rotorTops = [];
   this.rotorBottoms = [];
-
-  // Interval
   this.countdown = null;
-
-  // Number of days remaining
   this.daysRemaining = 0;
-
-  // Clock values as numbers
   this.clockValues = {};
-
-  // Clock values as strings
   this.clockStrings = {};
-
-  // Clock values as array
   this.clockValuesAsString = [];
   this.prevClockValuesAsString = [];
-
-  // Print Version
   console.log('FlipDown ' + this.version);
 }
 
-/**
-* @name FlipDown.prototype.start
-* @description Start the countdown
-* @author PButcher
-**/
-FlipDown.prototype.start = function() {
-
-  // Initialise the clock
-  if(!this.initialised) this.init();
-
-  // Set up the countdown interval
+FlipDown.prototype.start = function () {
+  if (!this.initialised) this.init();
   this.countdown = setInterval(this.tick.bind(this), 1000);
-
   return this;
-}
+};
 
-/**
-* @name FlipDown.prototype.ifEnded
-* @description Call a function once the countdown ends
-* @author PButcher
-* @param {function} cb - Callback
-**/
-FlipDown.prototype.ifEnded = function(cb) {
+FlipDown.prototype.ifEnded = function (cb) {
   cb();
-
   return this;
-}
+};
 
-/**
-* @name FlipDown.prototype.getTime
-* @description Get the time in seconds (unix timestamp)
-* @author PButcher
-**/
-FlipDown.prototype.getTime = function() {
+FlipDown.prototype.getTime = function () {
   return new Date().getTime() / 1000;
-}
+};
 
-/**
-* @name FlipDown.prototype.init
-* @description Initialise the countdown
-* @author PButcher
-**/
-FlipDown.prototype.init = function() {
-
+FlipDown.prototype.init = function () {
   this.initialised = true;
 
-  // Calculate how many digits the day counter needs
-  if(this.countdownEnded) {
+  if (this.countdownEnded) {
     this.daysremaining = 0;
   } else {
     this.daysremaining = Math.floor((this.epoch - this.now) / 86400).toString().length;
   }
+
   var dayRotorCount = this.daysremaining <= 2 ? 2 : this.daysremaining;
 
-  // Create and store rotors
-  for(var i = 0; i < dayRotorCount + 6; i++) {
+  for (var i = 0; i < dayRotorCount + 6; i++) {
     this.rotors.push(this.createRotor(0));
   }
 
-  // Create day rotor group
   var dayRotors = [];
-  for(var i = 0; i < dayRotorCount; i++) {
+
+  for (var i = 0; i < dayRotorCount; i++) {
     dayRotors.push(this.rotors[i]);
   }
-  this.element.appendChild(this.createRotorGroup(dayRotors));
 
-  // Create other rotor groups
+  this.element.appendChild(this.createRotorGroup(dayRotors));
   var count = dayRotorCount;
-  for(var i = 0; i < 3; i++) {
+
+  for (var i = 0; i < 3; i++) {
     var otherRotors = [];
-    for(var j = 0; j < 2; j++) {
+
+    for (var j = 0; j < 2; j++) {
       otherRotors.push(this.rotors[count]);
       count++;
     }
+
     this.element.appendChild(this.createRotorGroup(otherRotors));
   }
 
-  // Store and convert rotor nodelists to arrays
   this.rotorLeafFront = Array.from(document.getElementsByClassName('rotor-leaf-front'));
   this.rotorLeafRear = Array.from(document.getElementsByClassName('rotor-leaf-rear'));
   this.rotorTop = Array.from(document.getElementsByClassName('rotor-top'));
   this.rotorBottom = Array.from(document.getElementsByClassName('rotor-bottom'));
-
-  // Set initial values;
   this.tick();
   this.updateClockValues(true);
-
   return this;
-}
+};
 
-/**
-* @name FlipDown.prototype.createRotorGroup
-* @description Add rotors to the DOM
-* @author PButcher
-* @param {array} rotors - A set of rotors
-**/
-FlipDown.prototype.createRotorGroup = function(rotors) {
+FlipDown.prototype.createRotorGroup = function (rotors) {
   var rotorGroup = document.createElement('div');
   rotorGroup.className = 'rotor-group';
   var dayRotorGroupHeading = document.createElement('div');
@@ -157,14 +88,9 @@ FlipDown.prototype.createRotorGroup = function(rotors) {
   rotorGroup.appendChild(dayRotorGroupHeading);
   appendChildren(rotorGroup, rotors);
   return rotorGroup;
-}
+};
 
-/**
-* @name FlipDown.prototype.createRotor
-* @description Create a rotor DOM element
-* @author PButcher
-**/
-FlipDown.prototype.createRotor = function(v) {
+FlipDown.prototype.createRotor = function (v) {
   var rotor = document.createElement('div');
   var rotorLeaf = document.createElement('div');
   var rotorLeafRear = document.createElement('figure');
@@ -183,96 +109,63 @@ FlipDown.prototype.createRotor = function(v) {
   appendChildren(rotor, [rotorLeaf, rotorTop, rotorBottom]);
   appendChildren(rotorLeaf, [rotorLeafRear, rotorLeafFront]);
   return rotor;
-}
+};
 
-/**
-* @name FlipDown.prototype.tick
-* @description Calculate current tick
-* @author PButcher
-**/
-FlipDown.prototype.tick = function() {
-
-  // Get time now
+FlipDown.prototype.tick = function () {
   this.now = this.getTime();
-
-  // Between now and epoch
-  var diff = ((this.epoch - this.now) <= 0) ? 0 : this.epoch - this.now;
-
-  // Days remaining
+  var diff = this.epoch - this.now <= 0 ? 0 : this.epoch - this.now;
   this.clockValues.d = Math.floor(diff / 86400);
   diff -= this.clockValues.d * 86400;
-
-  // Hours remaining
   this.clockValues.h = Math.floor(diff / 3600);
   diff -= this.clockValues.h * 3600;
-
-  // Minutes remaining
   this.clockValues.m = Math.floor(diff / 60);
   diff -= this.clockValues.m * 60;
-
-  // Seconds remaining
   this.clockValues.s = Math.floor(diff);
-
-  // Update clock values
   this.updateClockValues();
-}
+};
 
-/**
-* @name FlipDown.prototype.updateClockValues
-* @description Update the clock face values
-* @author PButcher
-* @param {boolean} init - True if calling for initialisation
-**/
-FlipDown.prototype.updateClockValues = function(init = false) {
+FlipDown.prototype.updateClockValues = function () {
+  var _this = this;
 
-  // Build clock value strings
+  var init = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
   this.clockStrings.d = pad(this.clockValues.d, 2);
   this.clockStrings.h = pad(this.clockValues.h, 2);
   this.clockStrings.m = pad(this.clockValues.m, 2);
   this.clockStrings.s = pad(this.clockValues.s, 2);
-
-  // Concat clock value strings
   this.clockValuesAsString = (this.clockStrings.d + this.clockStrings.h + this.clockStrings.m + this.clockStrings.s).split('');
-
-
-  // Update rotor values
-  // Note that the faces which are initially visible are:
-  // - rotorLeafFront (top half of current rotor)
-  // - rotorBottom (bottom half of current rotor)
-  // Note that the faces which are initially hidden are:
-  // - rotorTop (top half of next rotor)
-  // - rotorLeafRear (bottom half of next rotor)
-  this.rotorLeafFront.forEach((el, i) => {
-    el.textContent = this.prevClockValuesAsString[i];
+  this.rotorLeafFront.forEach(function (el, i) {
+    el.textContent = _this.prevClockValuesAsString[i];
   });
-
-  this.rotorBottom.forEach((el, i) => {
-    el.textContent = this.prevClockValuesAsString[i];
+  this.rotorBottom.forEach(function (el, i) {
+    el.textContent = _this.prevClockValuesAsString[i];
   });
 
   function rotorTopFlip() {
-    this.rotorTop.forEach((el, i) => {
-      if(el.textContent != this.clockValuesAsString[i]) {
-        el.textContent = this.clockValuesAsString[i];
+    var _this2 = this;
+
+    this.rotorTop.forEach(function (el, i) {
+      if (el.textContent != _this2.clockValuesAsString[i]) {
+        el.textContent = _this2.clockValuesAsString[i];
       }
     });
   }
 
   function rotorLeafRearFlip() {
-    this.rotorLeafRear.forEach((el, i) => {
-      if(el.textContent != this.clockValuesAsString[i]) {
-        el.textContent = this.clockValuesAsString[i];
+    var _this3 = this;
+
+    this.rotorLeafRear.forEach(function (el, i) {
+      if (el.textContent != _this3.clockValuesAsString[i]) {
+        el.textContent = _this3.clockValuesAsString[i];
         el.parentElement.classList.add('flipped');
-        var flip = setInterval(function() {
+        var flip = setInterval(function () {
           el.parentElement.classList.remove('flipped');
           clearInterval(flip);
-        }.bind(this), 500);
+        }.bind(_this3), 500);
       }
     });
   }
 
-  // Init
-  if(!init) {
+  if (!init) {
     setTimeout(rotorTopFlip.bind(this), 500);
     setTimeout(rotorLeafRearFlip.bind(this), 500);
   } else {
@@ -280,30 +173,16 @@ FlipDown.prototype.updateClockValues = function(init = false) {
     rotorLeafRearFlip.call(this);
   }
 
-  // Save a copy of clock values for next tick
   this.prevClockValuesAsString = this.clockValuesAsString;
-}
+};
 
-/**
-* @name pad
-* @description Prefix a number with zeroes
-* @author PButcher
-* @param {string} n - Number to pad
-* @param {number} len - Desired length of number
-**/
 function pad(n, len) {
   n = n.toString();
   return n.length < len ? pad("0" + n, len) : n;
 }
 
-/**
-* @name appendChildren
-* @description Add multiple children to an element
-* @author PButcher
-* @param {object} parent - Parent
-**/
 function appendChildren(parent, children) {
-  children.forEach(el => {
+  children.forEach(function (el) {
     parent.appendChild(el);
   });
 }
